12192f5c817561bcf99b3a4094a0b6964f6f7e39
sKiZmm <patalinghugdarylle@gmail.com>
2025-10-26 23:28:50 +0800

    Changes made in LearningRoadmapTracker.xlsx, Day 8.md Added Day 9.md, text_stats.py, text_summary.py, text.txt

---
+++ Days/Day 9.md
@@ -0,0 +1,40 @@
+# 10-26-2025
+
+## 🕐 09:20 PM - Python
+**Platform:** [W3 Schools](https://www.w3schools.com/python/python_file_handling.asp)
+
+**What I Learned:**
+- Python FIle Handling
+- Python Read FIles
+- Python Write/Create Files
+- Python Delete Files
+
+**Notes:**
+> Python basics done, will learn File I/O from W3 Schools.
+
+> All done! Gonna create small projects now with the help of GPT suggesting them.
+
+> [Text File Statistics](..//Python%20Basic%20Project/Text%20File%20Statistics/text_stats.py)
+> ```text
+> “Text File Statistics” Script
+>
+> What it does:
+> 
+> Read a text file (say sample.txt)
+> 
+> Count number of lines, number of words, number of characters
+> 
+> Find the longest word or average word length
+> 
+> Write a short summary into another file (summary.txt)
+>```
+
+> Above is the suggested GPT project.
+
+> Holy hell this took a while but I did it! Feels so good, it may not be a clean/optimized code but hey it works!
+
+---
+
+## 🕐 11:27 PM - Break
+
+---
---
+++ Python Basic Project/Text File Statistics/text_stats.py
@@ -0,0 +1,68 @@
+text = open(r"Python Basic Project\Text File Statistics\text.txt", "rt")
+
+def count_lines(file: object) -> int:
+    count = 0
+    file.seek(0)
+
+    for lines in file:
+        count += 1
+    return count
+
+def count_words(file: object) -> int:
+    count = 0
+    file.seek(0)
+
+    for lines in file:
+        lines = lines.split()
+        count += len(lines)
+    return count
+
+def count_characters(file: object) -> int:
+    count = 0
+    file.seek(0)
+
+    for lines in file:
+        count += len(lines.rstrip("\n"))
+    return count
+
+def write_file(text: object) -> None:
+    with open(r"Python Basic Project\Text File Statistics\text_summary.txt", "w+t") as f:
+        f.write(text)
+        f.seek(0)
+        print(f.read())
+
+def find_longest_word(file: object) -> str:
+    longest_word = ''
+    file.seek(0)
+
+    for lines in file:
+        lines = lines.split()
+        for word in lines:
+            if len(word) > len(longest_word):
+                longest_word = word
+
+    return longest_word
+        
+def find_average(file: object, total_words: int) -> float:
+    file.seek(0)
+    sum = 0
+
+    for lines in file:
+        lines = lines.split()
+        for words in lines:
+            sum += len(words)
+    return sum / total_words
+
+print(f"text.txt contains:\n{text.read()}\n")
+
+lines = count_lines(text)
+words = count_words(text)
+characters = count_characters(text)
+longest_word = find_longest_word(text)
+average = find_average(text, words)
+summary = f"text.txt has {lines} lines\ntext.txt has {words} words\ntext.txt has {characters} characters\ntext.txt longest word is {longest_word}\ntext.txt has an average word length of {average:.2f}"
+
+write_file(summary)
+print("\nFile output saved as text_summary.txt")
+
+text.close()
