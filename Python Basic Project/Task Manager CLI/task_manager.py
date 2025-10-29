b5a58e3bfdb07a4e92c21f354204062c1c401e48
sKiZmm <patalinghugdarylle@gmail.com>
2025-10-29 09:58:15 +0800

    Renamed Day 9,10.md to Day 9,, 10, 11.md Changes made in task_manager.py, to-do.txt

--- Python Basic Project/Task Manager CLI/task_manager.py
+++ Python Basic Project/Task Manager CLI/task_manager.py
@@ -9,7 +9,7 @@ Write changes back to file.
 
 # Create simple GUI
 def menu() -> int:
-    print("\nHello, what would you like to do?\n")
+    print("Hello, what would you like to do?\n")
     print("1. Display To-Do list")
     print("2. Add Task")
     print("3. Edit Task")
@@ -44,7 +44,64 @@ def add_task():
     return None
 
 def edit_task():
-    return None
+    with open(r"Python Basic Project\Task Manager CLI\to-do.txt", "rt") as f:
+        i = 1
+        print('\n')
+        for line in f:
+            print(f"{i}. {line.strip()}")
+            i += 1
+        f.seek(0)
+        task_list = [ line.strip() for line in f ]
+
+    print("\nWhich task would you like to edit?")
+    while True:
+        task_line_str = input("Enter line number: ")
+
+        try:
+            task_line = int(task_line_str) - 1
+        except ValueError:
+            print("Please enter a number!")
+            continue
+
+        if task_line < 0 or task_line > len(task_list) - 1:
+            print(f"Please enter a number between 1-{len(task_list)}!")
+            continue
+        break
+
+    if "Completed" in task_list[task_line]:
+        print(f"\nMark \"{task_list[task_line][:-12]}\" as uncompleted?")
+        while True:
+            choice = input("Y/N: ").lower()
+            if choice != 'y' and choice != 'n':
+                print("Not a choice, please enter 'Y' or 'N'")
+                continue
+            break
+            
+        if choice == 'y':
+            task_list[task_line] = task_list[task_line][:-12]
+            print(f"\n\"{task_list[task_line]}\" marked as uncompleted!")
+        else:
+            return
+        
+    else:
+        print(f"\nMark \"{task_list[task_line]}\" as completed?")
+        while True:
+            choice = input("Y/N: ").lower()
+            if choice != 'y' and choice != 'n':
+                print("Not a choice, please enter 'Y' or 'N'")
+                continue
+            break
+
+        if choice == 'y':
+            task_list[task_line] += " - Completed"
+            print(f"\n\"{task_list[task_line][:-12]}\" marked as completed!")
+        else:
+            return
+    
+    with open(r"Python Basic Project\Task Manager CLI\to-do.txt", "wt") as f:
+            f.write('\n'.join(task_list))
+
+    return
 
 def delete_task():
     return None
